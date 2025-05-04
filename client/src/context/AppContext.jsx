94c7f385c952c5e8b016376e1666d22528870269
chetanapriya18 <chetanapriyavepada@gmail.com>
2025-05-04 08:44:33 +0530

    `Added Tailwind CSS postcss plugin, autoprefixer, and updated dependencies in package-lock.json and package.json. Updated AppContext to fetch all courses and calculate course ratings. Updated Navbar and CourseCard components to use new AppContext features.`

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,42 @@
-import { createContext } from "react";
+import { createContext , useState , useEffect} from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ( props) => {
 
   const currency = import.meta.env.VITE_CURRENCY
+  const navigate = useNavigate()
+
+  const [allCourses, setAllCourses] = useState([]);
+  const [isEducator, setIsEducator] = useState(true);
+
+    // Fetch All Courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  }
+
+  //Function to cal avg rating of course
+
+const calculateRating = (course)=>{
+  if(course.courseRatings.length===0){
+    return 0;
+  }
+  let totalRating = 0
+  course.courseRatings.forEach(rating=>{
+    totalRating+=rating.rating
+  })
+  return Math.floor(totalRating / course.courseRatings.length)
+}
+
+  useEffect(() => {
+    fetchAllCourses();
+  
+  }, []);
 
   const value = {
-    currency
+    currency , allCourses , navigate , calculateRating , isEducator, setIsEducator
   };
 
   return (
